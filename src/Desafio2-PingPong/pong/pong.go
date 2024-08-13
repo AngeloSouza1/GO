c3df42bbebf48fe58a49acc1e2674159a664a276
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-13 18:15:59 -0300

    feat: Desafio 'PingPong'

--- src/Desafio2-PingPong/pong/pong.go
+++ src/Desafio2-PingPong/pong/pong.go
@@ -3,17 +3,17 @@ package pong
 import "time"
 
 const (
-    Reset      = "\033[0m"
-    PongColor  = "\033[36m" 
+	Reset     = "\033[0m"
+	PongColor = "\033[36m"
 )
 
 func Pong(c chan<- string, done <-chan bool) {
-    for {
-        select {
-        case c <- PongColor + "pong" + Reset:
-            time.Sleep(1 * time.Second)
-        case <-done:
-            return
-        }
-    }
+	for {
+		select {
+		case c <- PongColor + "pong" + Reset:
+			time.Sleep(1 * time.Second)
+		case <-done:
+			return
+		}
+	}
 }
